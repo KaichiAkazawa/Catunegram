96ac5af9f7e4bc2b001929cd9f4310a658ead011
Kaichi Akazawa <kaichi@KaichinoMacBook-Air.local>
2020-11-15 17:12:17 +0900

    add new file

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '205ce6ee405d547ec6d08e03170c41986d22a2d73fd39a780519c2ac139c955a9f3e4dec25fb2b5ccbc485c1b61b76663f91e14a90379d5cb66af986663cbd2b'
